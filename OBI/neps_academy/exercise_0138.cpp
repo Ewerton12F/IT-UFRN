2779c1347e900d6626d018f14420abbfd7e4e3dd
Ewerton Freitas <ewerton12freitas@gmail.com>
2026-04-15 12:14:54 -0300

    exercícios aula 02

---
+++ OBI/neps_academy/exercise_0138.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Lendo a entrada do exercício
+    int L;
+    cin >> L;
+
+    // Seu código vai aqui
+    cout << (L * L);
+
+    return 0;
+}
---
+++ OBI/neps_academy/exercise_0154.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Seu código vai aqui
+    int X;
+    cin >> X;
+
+    if (0 <= X <= 1000) {
+        cout << X << " * 1 = " << X * 1 << "\n";
+        cout << X << " * 2 = " << X * 2 << "\n";
+        cout << X << " * 3 = " << X * 3 << "\n";
+        cout << X << " * 4 = " << X * 4 << "\n";
+        cout << X << " * 5 = " << X * 5 << "\n";
+        cout << X << " * 6 = " << X * 6 << "\n";
+        cout << X << " * 7 = " << X * 7 << "\n";
+        cout << X << " * 8 = " << X * 8 << "\n";
+        cout << X << " * 9 = " << X * 9 << "\n";
+        cout << X << " * 10 = " << X * 10 << "\n";
+    }
+    return 0;
+}
