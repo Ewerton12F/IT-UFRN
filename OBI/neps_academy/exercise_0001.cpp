14b40dac1a6e5897a5fb1b90f7a4bf994bdad4fa
Ewerton Freitas <ewerton12freitas@gmail.com>
2026-04-15 10:58:49 -0300

    exercise1

---
+++ OBI/neps_academy/exercise_0001.cpp
@@ -0,0 +1,10 @@
+// Since this is a special exercise, we have already solved it for you :). However, pay attention: the output must be EXACTLY as requested in the exercise statement.
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Ola Neps Academy!" << endl;
+
+    return 0;
+}
