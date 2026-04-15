5a7074d2f0b5759eead2cfb511ac0a885e97789b
Ewerton Freitas <ewerton12freitas@gmail.com>
2026-04-15 11:04:22 -0300

    aula 02

---
+++ OBI/aula02_variaveis1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int w = 5;
+    long long x = 5;
+    double y = 5.0;
+    string z = "5";
+    char c = '5';
+    bool b = true;
+    
+    cout << "Texto1";
+    cout << "Texto2" << "Texto3"; // Encadeamento
+    cout << "Texto4\n"; // Quebra de linha
+    cout << x; // O valor da variável x
+    cout << 10 + x;
+    cout << "\n";
+}
---
+++ OBI/aula02_variaveis3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y; // o mesmo que int x; int y;
+
+    cin >> x >> y; // input
+
+    string s;
+    cin >> s;
+}
