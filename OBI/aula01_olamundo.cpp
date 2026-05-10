0d1df8d82562263e310c2f36d8bc18e5655a13a3
Ewerton Freitas <ewerton12freitas@gmail.com>
2026-05-10 01:27:02 -0300

    obs and compiling commands

--- OBI/aula01_olamundo.cpp
+++ OBI/aula01_olamundo.cpp
@@ -1,6 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> // Inclusão de comandos úteis
+using namespace std; // Deixa o código mais curto
 
-int main() {
-    cout << "Olá, mundo!";
+int main() { // Delimita o começo do nosso programa
+    cout << "Olá, mundo!"; // Imprime "Olá, mundo" na tela
 }
+
+/*
+COMPILANDO O PROGRAMA
+
+g++ olamundo.cpp -o olamundo
+./olamundo
+
+*/
