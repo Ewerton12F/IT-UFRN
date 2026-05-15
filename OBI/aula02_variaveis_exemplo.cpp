3324ef55563364ac980c4eebcaeed7b2173591da
Ewerton Freitas <ewerton12freitas@gmail.com>
2026-05-15 04:00:19 -0300

    aula 02

---
+++ OBI/aula02_variaveis_exemplo.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 6;
+    int z = x + y;
+    cout << z << "\n";
+    x = 10;
+    z = x + y;
+    cout << z << "\n";
+    y = 0;
+    z = x + y;
+    cout << z << "\n";
+}
